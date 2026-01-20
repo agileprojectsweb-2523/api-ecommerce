36e80c4efed8c9a863bb7738191666a4671a65bb
Patrick Siqueira <patrickgsiqueira@hotmail.com>
2026-01-20 13:13:32 -0300

    Enable SSL for production DB connection

--- src/config/database.js
+++ src/config/database.js
@@ -27,5 +27,11 @@ module.exports = {
     port: process.env.DB_PORT || 5432,
     dialect: 'postgres',
     logging: false,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
   },
 };
